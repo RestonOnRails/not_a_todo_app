b84555aed2a3e668600b8542dddc86df096404a2
Melissa Miller <lis.d.miller@gmail.com>
2016-09-17 15:30:28 -0400

    Updated schema to remove unused properties

--- db/migrate/20160915100635_create_to_dos.rb
+++ db/migrate/20160915100635_create_to_dos.rb
@@ -2,7 +2,6 @@ class CreateToDos < ActiveRecord::Migration[5.0]
   def change
     create_table :to_dos do |t|
       t.string :item
-      t.boolean :complete
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 20160915100635) do
 
   create_table "to_dos", force: :cascade do |t|
     t.string   "item"
-    t.boolean  "complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
